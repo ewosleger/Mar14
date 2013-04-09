339bec6da9e9aeee5d5f0fb69707d91311c7be24
Edward Wosleger <edw@vandelayinteractive.com>
2013-04-09 19:21:13 -0400

    updated Mar14
    swipe right to change video in queue

---
+++ View.h
@@ -0,0 +1,13 @@
+//
+//  View.h
+//  Mar14
+//
+//  Created by Edward Wosleger on 3/14/13.
+//  Copyright (c) 2013 Edward Wosleger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View : UIView
+
+@end
---
+++ View.m
@@ -0,0 +1,31 @@
+//
+//  View.m
+//  Mar14
+//
+//  Created by Edward Wosleger on 3/14/13.
+//  Copyright (c) 2013 Edward Wosleger. All rights reserved.
+//
+
+#import "View.h"
+
+@implementation View
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
